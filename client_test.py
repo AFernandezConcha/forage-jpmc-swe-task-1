fc1185aa0b8c5e1abd2944b1750103455599860d
AFernandezConcha <FernandezConcha.Alejandro@gmail.com>
2023-06-02 15:39:53 +0200

    Bonus task 1: Tests

--- client_test.py
+++ client_test.py
@@ -2,12 +2,28 @@ import unittest
 from client3 import getDataPoint
 
 class ClientTest(unittest.TestCase):
+
+  def assertion_getDataPoint(self, quotes):
+    for quote in quotes:
+      self.assertEqual(
+        getDataPoint(quote),
+        (
+          quote['stock'], 
+          quote['top_bid']['price'],
+          quote['top_ask']['price'], 
+          (quote['top_bid']['price'] + quote['top_ask']['price'])/2,
+        )
+      )
+    
+    
   def test_getDataPoint_calculatePrice(self):
     quotes = [
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    self.assertion_getDataPoint(quotes)
+
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,6 +31,7 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    self.assertion_getDataPoint(quotes)
 
 
   """ ------------ Add more unit tests ------------ """
